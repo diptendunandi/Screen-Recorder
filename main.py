845fce76ac118799a62e3e111524445d6aca9292
Diptendu Nandi <67785237+diptendunandi@users.noreply.github.com>
2022-01-01 20:21:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import datetime
+
+from PIL import ImageGrab
+import numpy as np
+import cv2
+from win32api import GetSystemMetrics
+
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
+time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+file_name = f'{time_stamp}.mp4'
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+captured_video = cv2.VideoWriter(file_name, fourcc, 20.0, (width, height))
+
+webcam = cv2.VideoCapture(1)
+
+while webcam.isOpened():
+    ret, frame = webcam.read()
+    while True:
+        img = ImageGrab.grab(bbox=(0, 0, width, height))
+        img_np = np.array(img)
+        img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+        ret,frame = webcam.read()
+        fr_height, fr_width, _ = frame.shape
+        img_final[0:fr_height, 0: fr_width, :] = frame[0: fr_height, 0: fr_width, :]
+        cv2.imshow('Secret Capture', img_final)
+        captured_video.write(img_final)
+        if cv2.waitKey(10) == ord('q'):
+            break
+# When everything done, release the capture
+webcam.release()
+cv2.destroyAllWindows()
